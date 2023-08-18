79c19dbb709c2a909681468b894a7dea19e7211d
Fredrik Rofors <fredrik_rofors@hotmail.com>
2023-08-18 16:55:11 +0200

    Added client side fetch + filtering of sessions

--- app/conference/sessions/page.tsx
+++ app/conference/sessions/page.tsx
@@ -1,33 +1,90 @@
+'use client'
+
 import style from './sessions.module.css';
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 
+export default function Sessions() {
+    const [loading, setLoading] = useState<boolean>(true);
+    const [sessions, setSessions] = useState([]);
+    const [tracks, setTracks] = useState([]);
+    const [selectedTracks, setSelectedTracks] = useState<string[]>([]);
 
-async function fetchSessions() {
-    const url = "https://raw.githubusercontent.com/adhithiravi/consuming-graphql-apollo/master/api/data/sessions.json"
-    const response = fetch(url,
-        {
-            // Fetch server-side on each request
-            cache: "no-store"
-        });
+    useEffect(() => {
+        console.log('useEffet::selectedTracks=', selectedTracks);
 
-    const data = (await response).json();
-    return data;
-}
+        if (loading) {
+            const url = "https://raw.githubusercontent.com/adhithiravi/consuming-graphql-apollo/master/api/data/sessions.json"
+            fetch(url)
+                .then((res) => res.json())
+                .then((data) => {
+                    setSessions(data.sessions)
+                    const allTracks = data.sessions.map((session: any) => session.track)
+                        .filter((value: any, index: any, self: any) => self.indexOf(value) === index);
+                    setTracks(allTracks);
 
+                    setLoading(false);
+                });
+        }
+    });
 
-export default async function Sessions() {
-    const data = await fetchSessions();
     const renderingTime = new Date();
+
+    const toogleSelectedTrack = (trackName: string) => {
+        let st = [...selectedTracks];
+        let item = st.find((track) => track === trackName);
+
+        if (item) {
+            // if track already in the list, remove the track
+            setSelectedTracks(st.filter((track) => track !== trackName));
+        } else {
+            // if not, add the track
+            st.push(trackName);
+            setSelectedTracks(st);
+        }
+    }
 
     return (
         <>
-            <h1>Enjoy our {data.sessions.length} enlightning sessions</h1>
-            <p>Last rendered: {`${renderingTime.toLocaleDateString()} ${renderingTime.toLocaleTimeString()}`}</p>
-            {data.sessions.map((speaker: any) => {
-                return (<div key={speaker.id} className={style.speaker}>
-                    <h2>{speaker.title}</h2>
-                    <p>{speaker.description}</p>
-                </div>)
-            })}
+            {loading && <h1>Loading the sessions...</h1>}
+            {!loading && (
+                <>
+                    <h1>Find your favourite sessions among all {sessions.length} sessions</h1>
+                    <p>Last rendered: {`${renderingTime.toLocaleDateString()} ${renderingTime.toLocaleTimeString()}`}</p>
+                    <div className={style.trackHeader}>
+                        {tracks.map((track: string) => {
+                            let selectedTrack = selectedTracks.find((trackName) => trackName === track);
+
+                            return (
+                                track &&
+                                <div key={track} className={`${style.track} ${selectedTrack ? style.trackSelected : ''}`} onClick={() => toogleSelectedTrack(track)}>{track}</div>
+                            )
+                        })}
+                    </div>
+                    {sessions.map((session: any) => {
+
+                        // is there a filter set? 
+                        if (selectedTracks.length > 0) {
+
+                            // then only include sessions matching that filter
+                            if (selectedTracks.find((track) => track === session.track)) {
+                                return (<div key={session.id} className={style.speaker}>
+                                    <h2>{session.title}</h2>
+                                    <h3>{session.track}</h3>
+                                    <p>{session.description}</p>
+                                </div>)
+                            }
+                        } else {
+                            // when no filter is set, include all sessions
+                            return (<div key={session.id} className={style.speaker}>
+                                <h2>{session.title}</h2>
+                                <h3>{session.track}</h3>
+                                <p>{session.description}</p>
+                            </div>)
+                        }
+                    })}
+                </>
+            )}
         </>
     );
 }
